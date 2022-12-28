a1d804d4a6ff71bf8de0080c503d5f4ba04be9d5
Sudipa Acharya <sudipaacharya351@gmail.com>
2022-12-28 14:11:32 +0530

    code added

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,27 @@
-import React from 'react'
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
 const App = () => {
-
-
+  const submitButton = () => {
+    console.log("form submitted");
+  };
   return (
     <div id="main">
-      <form>
-        <label htmlFor='name'>Name</label>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+        }}
+      >
+        <label htmlFor="name">Name</label>
         <input id="name" type={"text"} />
 
         <br />
         <br />
-        <button type='submit'>Submit</button>
+        <button type="submit" onClick={submitButton}>
+          Submit
+        </button>
       </form>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
